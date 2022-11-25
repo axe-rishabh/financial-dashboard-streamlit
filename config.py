d00507add87d7831c71691ab06d63a8f4c6cd65c
axe-rishabh <93008249+axe-rishabh@users.noreply.github.com>
2022-11-25 23:47:10 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,26 @@
+TWITTER_CONSUMER_KEY = 'GZZaXB6d9IICW6Dwnnpnd3stc'
+TWITTER_CONSUMER_SECRET = 'WROk2NtZMIeGDlTfNYmNbVrbBZCKpOsnlp0PUstLLAzG8bGebD'
+TWITTER_ACCESS_TOKEN = '1363856747564130311-3xOt1on5j8sgaIo4ZtpPns6XBAsHUx'
+TWITTER_ACCESS_TOKEN_SECRET = 'BlgvOhyJps1UzRo09BNa9HY79RWkNumpJgQtv2zwGormQ'
+
+TWITTER_USERNAMES = [
+    'traderstewie',
+    'the_chart_life',
+    'canuck2usa',
+    'sunrisetrader',
+    'tmltrader'
+]
+
+DB_HOST = '127.0.0.1'
+DB_USER = 'postgres'
+DB_PASS = 'password'
+DB_NAME = 'twitter'
+
+
+'''
+api key: 'GZZaXB6d9IICW6Dwnnpnd3stc'
+api key secret: 'WROk2NtZMIeGDlTfNYmNbVrbBZCKpOsnlp0PUstLLAzG8bGebD'
+bearer token: 'AAAAAAAAAAAAAAAAAAAAAF3ijgEAAAAA19YxcL%2FBu6mhgrFfLrx1rIkes0U%3DsLEmZxjirG7aH9XQMR8fNs6AYMsDv9gw27vQvrj922hLhj0HI2'
+client id: 
+
+'''
