ee2b0396a7f3f62c3a7ec615caad44e8e23c66a1
axe-rishabh <93008249+axe-rishabh@users.noreply.github.com>
2022-12-23 00:01:53 +0530

    removed all the options except twitter, stocktwit

--- streamlit_dashboard.py
+++ streamlit_dashboard.py
@@ -14,7 +14,8 @@ api = tweepy.API(auth)
 # connection = psycopg2.connect(host=config.DB_HOST, database=config.DB_NAME, user=config.DB_USER, password=config.DB_PASS)
 # cursor = connection.cursor(cursor_factory=psycopg2.extras.DictCursor)
 
-option = st.sidebar.selectbox("Which Dashboard?", ('twitter', 'wallstreetbets', 'stocktwits', 'chart', 'pattern'), 0)
+# option = st.sidebar.selectbox("Which Dashboard?", ('twitter', 'wallstreetbets', 'stocktwits', 'chart', 'pattern'), 0)
+option = st.sidebar.selectbox("Which Dashboard?", ('twitter', 'stocktwits'), 0)
 
 st.header(option)
 
