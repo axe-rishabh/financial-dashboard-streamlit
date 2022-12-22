5b2abc56b6278ab9cc0659aa797b4721ea5b6c0f
axe-rishabh <93008249+axe-rishabh@users.noreply.github.com>
2022-12-22 23:58:24 +0530

    changed default value in line 17 from 3 to 0

--- streamlit_dashboard.py
+++ streamlit_dashboard.py
@@ -14,7 +14,7 @@ api = tweepy.API(auth)
 # connection = psycopg2.connect(host=config.DB_HOST, database=config.DB_NAME, user=config.DB_USER, password=config.DB_PASS)
 # cursor = connection.cursor(cursor_factory=psycopg2.extras.DictCursor)
 
-option = st.sidebar.selectbox("Which Dashboard?", ('twitter', 'wallstreetbets', 'stocktwits', 'chart', 'pattern'), 3)
+option = st.sidebar.selectbox("Which Dashboard?", ('twitter', 'wallstreetbets', 'stocktwits', 'chart', 'pattern'), 0)
 
 st.header(option)
 
